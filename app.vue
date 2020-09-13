03a46b843127bae7c7a6722c0ae07f181423105b
Avinash-Singh-1998 <52065351+Avinash-Singh-1998@users.noreply.github.com>
2020-09-13 23:06:44 +0530

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,14 @@
+new Vue({
+el: '#app',
+data: {
+name: 'avinash.io'
+},
+filter: {
+Upper(value) {
+return value.toUpperCase();
+}
+}
+});
+<div id app="app">
+<span>{{name | Upper}}</span>
+</div>
